4aefc826b0541eda5c8646e816b8772c86ea2549
MoriamAkterSwarna <moriam15-12880@diu.edu.bd>
2023-06-17 21:57:27 +0600

    navbar added

---
+++ src/app/blog/page.jsx
@@ -0,0 +1,14 @@
+export const metadata = {
+    title: "ZET - Blog",
+  };
+
+
+const page = () => {
+    return (
+        <div>
+            <h2>Blog</h2>
+        </div>
+    );
+};
+
+export default page;
---
+++ src/app/partner/page.jsx
@@ -0,0 +1,13 @@
+export const metadata = {
+    title: "ZET - Partner With Us",
+  };
+
+const PartnerPage = () => {
+    return (
+        <div>
+            <h2>Partner with Us</h2>
+        </div>
+    );
+};
+
+export default PartnerPage;
